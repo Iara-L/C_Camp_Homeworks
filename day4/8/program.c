5c528606a8dab81c03022ba73a7e623e44ef5e10
Iaroslava L <66276044+Iara-L@users.noreply.github.com>
2022-06-05 12:39:46 +0300

    Add files via upload

---
+++ day4/8/program.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void swap(int* num1, int* num2);
+
+int main(void){
+    int num1, num2;
+    
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    printf("Before swap: Num1 = %d, Num2 = %d\n", num1, num2);
+    
+    swap(&num1, &num2);
+
+    printf("After swap: Num1 = %d, Num2 = %d\n", num1, num2);
+
+    return 0;
+}
